d46e642cb607ce40a80632bf2bc20c0a340625eb
Anastasiya Akimova <anastasiyaaki171@gmail.com>
2021-05-24 03:05:48 +0400

    addorder entity

---
+++ src/main/java/com/akimova/example/model/Order.java
@@ -0,0 +1,45 @@
+package com.akimova.example.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Book book;
+
+    public Order() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+}
