93cbce08936f2ee61626cc4c13a91f7411fac344
Anastasia <anastasiyaaki171@gmail.com>
2022-07-22 17:09:38 +0300

    Rename files, change types

--- src/main/java/com/akimova/example/repository/BookRepository.java
+++ src/main/java/com/akimova/example/repository/BookRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "book", path = "book")
-public interface BookRepo extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
     Book findBookById(Long id);
 
     Book findBookByTitle(String title);
