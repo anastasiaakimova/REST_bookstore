9ce0c7cf8bedf29b95d04953b20c54bd3e978d50
Anastasia <anastasiyaaki171@gmail.com>
2022-07-22 17:11:01 +0300

    Add lombock data

--- src/main/java/com/akimova/example/model/User.java
+++ src/main/java/com/akimova/example/model/User.java
@@ -1,10 +1,13 @@
 package com.akimova.example.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Table(name = "users")
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,61 +26,11 @@ public class User {
     @Column(name = "email", nullable = false)
     private String email;
 
-    @OneToMany(targetEntity=Comment.class, mappedBy = "user")
+    @OneToMany(targetEntity = Comment.class, mappedBy = "user")
     private List<Comment> comments;
 
-    @OneToMany(targetEntity=Order.class, mappedBy = "user")
+    @OneToMany(targetEntity = Order.class, mappedBy = "user")
     private List<Order> orders;
-
-    public User() { }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public List<Comment> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<Comment> comments) {
-        this.comments = comments;
-    }
-
-    public List<Order> getOrders() {
-        return orders;
-    }
-
-    public void setOrders(List<Order> orders) {
-        this.orders = orders;
-    }
 
     @Override
     public String toString() {
